5cbdc6fb0939bfa7bbbe6e0ab47b408f096c9f42
TIY <cheriehsu@cherieteki-MacBook-Air.local>
2014-06-17 11:59:01 +0800

    2014-6-17 ver:1 on gh-pages

--- apply/fb.js
+++ apply/fb.js
@@ -1,11 +1,61 @@
 var userid;
 var ACCESS_TOKEN;
 
+// This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+        var IfLoggedInDiv=document.getElementById("if-logged-in");
+        IfLoggedInDiv.style.display="inline-block";
+        var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
+        IfNotLoggedInDiv.style.display="none";
+      //testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      //document.getElementById('status').innerHTML = 'Please log ' +
+      //  'into this app.';
+        var IfLoggedInDiv=document.getElementById("if-logged-in");
+        IfLoggedInDiv.style.display="none";
+        var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
+        IfNotLoggedInDiv.style.display="inline-block";
+        FB.logout(function(response){
+            location.reload();  // refresh
+        });
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      //document.getElementById('status').innerHTML = 'Please log ' +
+      //  'into Facebook.';
+        var IfLoggedInDiv=document.getElementById("if-logged-in");
+        IfLoggedInDiv.style.display="none";
+        var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
+        IfNotLoggedInDiv.style.display="inline-block";
+        FB.logout(function(response){
+            location.reload();  // refresh
+        });
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
 window.fbAsyncInit = function () {//facebook init
     
 //輸入基本的Facebook init的狀態，與Facebook 連接，包括APP ID的設定
     FB.init({
-        appId : '322780024547098',                    // App ID from the app dashboard  // wp2014s_final_project:322780024547098  // Lab:699947756683654
+        appId : '699947756683654',                    // App ID from the app dashboard  // wp2014s_final_project:322780024547098  // Lab:699947756683654
         cookie  : true,                                 // Allowed server-side to fetch fb auth cookie
         status  : true,                                 // Check Facebook Login status
         xfbml   : true,                                 // Look for social plugins on the page
@@ -17,6 +67,7 @@ window.fbAsyncInit = function () {//facebook init
         statusChangeCallback(response);
     });
 
+
     // define the events when login status changed.
     FB.Event.subscribe('auth.login', function(response) {
 
@@ -25,12 +76,6 @@ window.fbAsyncInit = function () {//facebook init
             IfLoggedInDiv.style.display="inline-block";
             var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
             IfNotLoggedInDiv.style.display="none";
-
-            FB.api('/me', function(response) {
-                //    console.log('Good to see you, ' + response.name + '.');
-                    var UserName = document.getElementById("user_name");
-                    UserName.innerHTML=response.name;
-            });
 
             // get access token
             ACCESS_TOKEN=response.authResponse.accessToken;
@@ -63,12 +108,6 @@ window.fbAsyncInit = function () {//facebook init
                 IfLoggedInDiv.style.display="inline-block";
                 var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
                 IfNotLoggedInDiv.style.display="none";
-                
-                FB.api('/me', function(response) {
-                //    console.log('Good to see you, ' + response.name + '.');
-                    var UserName = document.getElementById("user_name");
-                    UserName.innerHTML=response.name;
-                });
                 
             } else {
                 console.log('User cancelled login or did not fully authorize.');
